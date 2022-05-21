e1c239341cd4536b5c6ddb74a0e4b8b7dec5969e
Jeremy Gurr <jeremy.gurr@gmail.com>
2022-05-20 20:30:38 -0700

    m

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -4040,6 +4040,8 @@ static tileidx_t _tileidx_player_species_base(const species_type species)
             return TILEG_SP_DJINNI;
         case SP_METEORAN:
             return TILEG_SP_METEORAN;
+        case SP_HYDRA:
+            return TILEG_SP_HYDRA;
         default:
             return TILEP_ERROR;
     }
