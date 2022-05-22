147bd55e5a076c19f3abfa0b7735a0b4239dc9b5
Jeremy Gurr <jeremy.gurr@gmail.com>
2022-05-21 19:23:57 -0700

    Closer to getting hydra head icon appearing. Works with load and save, but not when gaining exp

--- crawl-ref/source/tilepick-p.cc
+++ crawl-ref/source/tilepick-p.cc
@@ -19,6 +19,7 @@
 #include "tilepick.h"
 #include "transform.h"
 #include "traps.h"
+#include "mpr.h"
 
 static tileidx_t _modrng(int mod, tileidx_t first, tileidx_t last)
 {
@@ -592,6 +593,8 @@ static int _draconian_colour(int race, int level)
 
 tileidx_t tilep_species_to_base_tile(int sp, int level)
 {
+    int heads = you.props[NUM_HEADS_KEY].get_int();
+    dprf("tilep_species_to_base_tile: heads: %d", heads);
     switch (sp)
     {
     case SP_HUMAN:
@@ -670,7 +673,7 @@ tileidx_t tilep_species_to_base_tile(int sp, int level)
     case SP_BARACHI:
         return TILEP_BASE_BARACHI;
     case SP_HYDRA:
-        return TILEP_BASE_HYDRA;
+        return TILEP_BASE_HYDRA + min(heads - 1, 4);
     case SP_GNOLL:
         return TILEP_BASE_GNOLL;
     case SP_DJINNI:
